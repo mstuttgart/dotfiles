c77dbb95ae9b494df57b44567f27b85616e51221
Michell Stuttgart <michellstut@gmail.com>
2019-11-06 09:54:44 -0300

    Uncomment Chrome install command

--- setup-apps.sh
+++ setup-apps.sh
@@ -61,8 +61,8 @@ done
 
 # Install Chrome ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo dpkg -i google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # The $? get result of last command
 check_return_code $? "Google Chrome"
