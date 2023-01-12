4c264a53b56e7db0ae0276b650aaf140660b7598
Michell Stuttgart <michellstut@gmail.com>
2023-01-11 22:51:55 -0300

    Add new scripts and tags to roles

---
+++ bin/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# script based in: https://github.com/sloria/dotfiles/blob/master/bin/dot-update
+
+# Update local dev environment using ansible-playbook.
+# Optionally pass role names.
+set -e
+
+if [[ $# -eq 0 ]]; then
+  # Run all roles
+  echo "Updating local dev environment..."
+  ansible-playbook -i hosts playbooks/setup.yml --ask-become-pass --skip-tags "optional"
+
+else
+  echo "Updating local dev environment... (--tags $@)"
+  ansible-playbook -i hosts playbooks/setup.yml --ask-become-pass --tags $@
+
+fi
+
+# Display a notification
+notify-send 'Update complete' 'Successfully updated dev environment'
--- scripts/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source .venv/bin/activate
-
-ansible-playbook -i hosts playbooks/setup.yml --ask-become-pass -e 'ansible_python_interpreter=/usr/bin/python3'
-
-deactivate
