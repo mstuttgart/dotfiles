d33a488763ddb648bc180b0bb7964f8729cebd76
Michell Stuttgart <michellstut@gmail.com>
2023-01-27 11:27:49 -0300

    Update odoo aliases

--- roles/odoo/files/aliases.bash
+++ roles/odoo/files/aliases.bash
@@ -1,6 +1,4 @@
 # Odoo alises
 
-alias odoop="cd ~/Workspace/odoo"
-
 # Update branchs from hotfix
 alias ghotfix="git stash && git checkout main && git pull --rebase origin main && git checkout develop && git rebase origin/main && git pull --rebase origin develop && git push origin develop"
