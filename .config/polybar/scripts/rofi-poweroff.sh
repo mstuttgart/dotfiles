983440b9cebd861b32112451ba284be15f69142d
Michell Stuttgart <michellstut@gmail.com>
2023-05-15 18:01:14 -0300

    Fix logout option in rofi

--- .config/polybar/scripts/rofi-poweroff.sh
+++ .config/polybar/scripts/rofi-poweroff.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 rofi_poweroff_menu () {
-    echo -e "Logout\nReboot\nShutdown" | rofi -font "FiraCode Nerd Font 11" -show drun -show-icons -width 20 -l 3 -dmenu -i
+    echo -e "Logout\nReboot\nShutdown" | rofi -font "JetBrainsMono Nerd Font 11" -show drun -show-icons -width 20 -l 3 -dmenu -i
 }
 
 chosen=$(rofi_poweroff_menu)
 
 if [[ $chosen = "Logout" ]]; then
-  systemctl logout
+  i3-msg exit
 elif [[ $chosen = "Shutdown" ]]; then
 	systemctl poweroff
 elif [[ $chosen = "Reboot" ]]; then
