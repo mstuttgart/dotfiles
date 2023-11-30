0ff3a857fb4ab33d40566215b146ef8b895ad4ab
Michell Stuttgart <michellstut@gmail.com>
2023-11-30 12:00:01 -0300

    Daily backup: qui 30 nov 2023 12:00:01 -03

--- .config/nvim/lua/plugins/lsp/cmp.lua
+++ .config/nvim/lua/plugins/lsp/cmp.lua
@@ -23,12 +23,6 @@ local plugins = {
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
 
-    -- local has_words_before = function()
-    --   unpack = unpack or table.unpack
-    --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-    -- end
-
     cmp.setup {
       completion = {
         keyword_length = 2,
@@ -48,28 +42,6 @@ local plugins = {
         ["<C-e>"] = cmp.mapping.abort(),          -- close completion window
         ["<Esc>"] = cmp.mapping.close(),
         ["<CR>"] = cmp.mapping.confirm { select = false },
-        -- ["<Tab>"] = cmp.mapping(function(fallback)
-        --   if cmp.visible() then
-        --     cmp.select_next_item()
-        --     -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-        --     -- that way you will only jump inside the snippet region
-        --   elseif has_words_before() then
-        --     cmp.complete()
-        --   elseif luasnip.expand_or_jumpable() then
-        --     luasnip.expand_or_jump()
-        --   else
-        --     fallback()
-        --   end
-        -- end, { "i", "s" }),
-        -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-        --   if cmp.visible() then
-        --     cmp.select_prev_item()
-        --   elseif luasnip.jumpable(-1) then
-        --     luasnip.jump(-1)
-        --   else
-        --     fallback()
-        --   end
-        -- end, { "i", "s" }),
       },
 
       -- sources for autocompletion
--- .config/nvim/lua/plugins/lsp/lsp.lua
+++ .config/nvim/lua/plugins/lsp/lsp.lua
@@ -29,12 +29,6 @@ local plugins = {
       }
     end
 
-    -- configure javascript and typescript
-    -- lspconfig["tsserver"].setup {
-    --   filetypes = { "javascript", "typescript", "typescriptreact", "typescript.tsx" },
-    --   root_dir = function() return vim.loop.cwd() end
-    -- }
-
     -- configure lua server (with special settings)
     lspconfig["lua_ls"].setup {
       capabilities = capabilities,
