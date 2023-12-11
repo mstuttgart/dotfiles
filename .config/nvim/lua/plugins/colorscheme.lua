e68795f7e67e7163c86bfd4dbc936965937efeae
Michell Stuttgart <michellstut@gmail.com>
2023-12-11 12:00:01 -0300

    Daily backup: seg 11 dez 2023 12:00:01 -03

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -18,9 +18,26 @@ local plugins = {
       require("everforest").setup {
         italics = true,
       }
-      -- vim.o.background = "dark"
+      vim.o.background = "light"
       vim.cmd.colorscheme "everforest"
     end,
+  },
+  {
+    "maxmx03/solarized.nvim",
+    lazy = true,
+    -- priority = 1000,
+    -- config = function()
+    --   vim.o.background = "dark" -- or 'light'
+    --   vim.cmd.colorscheme "solarized"
+    -- end,
+  },
+  {
+    "shaunsingh/nord.nvim",
+    lazy = true,
+    -- priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme "nord"
+    -- end,
   },
 }
 
