b9956dd3244bff94dba063ff4fb6ff1c89510d10
Michell Stuttgart <michellstut@gmail.com>
2024-04-27 22:21:14 -0300

    Add lazygit plugin nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,44 +3,83 @@
 
 local opt = vim.opt
 
-opt.laststatus = 3 -- global statusline
-opt.showmode = false
+-- line numbers
+opt.relativenumber = false -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
-opt.clipboard = "unnamedplus"
-opt.cursorline = true
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
 
--- Indenting
-opt.expandtab = true
-opt.shiftwidth = 2
-opt.smartindent = true
-opt.tabstop = 2
-opt.softtabstop = 2
+-- line wrapping
+opt.wrap = false -- disable line wrapping
 
-opt.fillchars = { eob = " " }
-opt.ignorecase = true
-opt.smartcase = true
-opt.mouse = "a"
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
--- Numbers
-opt.number = true
-opt.numberwidth = 2
-opt.ruler = false
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
 
--- disable nvim intro
-opt.shortmess:append "sI"
+-- appearance
 
-opt.signcolumn = "yes"
-opt.splitbelow = true
-opt.splitright = true
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
-opt.timeoutlen = 400
-opt.undofile = true
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
--- interval for writing swap file to disk, also used by gitsigns
-opt.updatetime = 250
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
--- disable some default providers
-for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
-  vim.g["loaded_" .. provider .. "_provider"] = 0
-end
+-- clipboard
+opt.clipboard = "unnamedplus" -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
+
+-- opt.laststatus = 3 -- global statusline
+-- opt.showmode = false
+
+-- opt.clipboard = "unnamedplus"
+-- opt.cursorline = true
+
+-- -- Indenting
+-- opt.expandtab = true
+-- opt.shiftwidth = 2
+-- opt.smartindent = true
+-- opt.tabstop = 2
+
+-- opt.fillchars = { eob = " " }
+-- opt.ignorecase = true
+-- opt.smartcase = true
+-- opt.mouse = "a"
+
+-- -- Numbers
+-- opt.number = true
+-- -- opt.numberwidth = 1
+-- opt.ruler = false
+
+-- -- disable nvim intro
+-- opt.shortmess:append "sI"
+
+-- opt.signcolumn = "yes"
+-- opt.splitbelow = true
+-- opt.splitright = true
+-- opt.termguicolors = true
+-- opt.timeoutlen = 400
+-- opt.undofile = true
+
+-- -- interval for writing swap file to disk, also used by gitsigns
+-- opt.updatetime = 250
+
+-- -- disable some default providers
+-- for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
+--   vim.g["loaded_" .. provider .. "_provider"] = 0
+-- end
 
